ef74eed229e3bea1020b848051cfec640ea06a03
brucechanjianle <jianle001@e.ntu.edu.sg>
2020-09-30 09:42:20 +0800

    update simple marker node with comments

--- src/simple_marker_node.cpp
+++ src/simple_marker_node.cpp
@@ -4,10 +4,13 @@ const std::string RosNodeName = "simple_marker_node";
 
 int main(int argc, char ** argv)
 {
+    // Initialize rosnode
     ros::init(argc, argv, RosNodeName);
 
+    // Instantiate simple maker object
     simple_marker node("simple_marker");
 
+    // Run node
     node.start();
 
     return 0;
